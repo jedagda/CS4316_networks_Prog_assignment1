e47b8ff9096698947416ad1164277d298b10ce68
Daniela Garcia Hernandez <danielagarciaher98@gmail.com>
2020-04-06 17:28:59 -0600

    receiver code

--- StarterCode-GBN_SR/Receiver.py
+++ StarterCode-GBN_SR/Receiver.py
@@ -10,6 +10,34 @@ RECEIVER_ADDR = ('localhost', 8080)
 # Receive packets from the sender
 def receive(sock, filename):
    # fill your code here
+    try:
+        f = open(filename, 'wb2')
+    except IOError:
+        print('could not open: ', filename)
+        return
+    
+    n=0
+
+    while True:
+        p = udt.rcv(sock)
+        if not p:
+            break
+        sequence_num = packet.extract(p)
+        data = packet.extract(p)
+
+        if sequence_num == n:
+    	    p = packet.make(n)
+    	    print('received packet')
+    	    address = udt.rcv(sock)
+    	    udt.send(p,sock,address) 
+    	    n = n+1
+    	    f.write(data)
+        else:
+            p = packet.make(n-1)
+            udt.send(p,sock,address)
+
+    file.close
+
 
 # Main function
 if __name__ == '__main__':
